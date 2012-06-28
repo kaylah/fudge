74483d199af33b6d3e5a02051ce5ecac7d2c3b1d
Kayla Hennegin <kayla@bellafaithdesign.com>
2012-06-28 13:44:57 -0500

    Initial js/css files
    Added basic markup/js/styles for 3d text effect

---
+++ fudge.js
@@ -0,0 +1,39 @@
+/**
+ * Fudge
+ * Text Effects!
+ */
+
+(function( $ ){
+
+  $.fn.fudge = function( options ){
+
+      // Options
+      var settings = $.extend({
+          'effect': '3d'
+      }, options);
+      
+      // Utility Functions
+      var wrapper = function(id, whatever, wheatever){
+      };
+
+      // Individual Effects
+      var threeD = function(){
+          alert('hi');
+          wrapper(asdf asdf asdf);
+      },
+          clotheslines = function(){
+          },
+          
+          alarmClock = function(){
+          };
+      
+
+      switch(settings.effect){
+          case '3d': threeD();
+              };                  
+
+  };
+})( jQuery );
+
+   
+$('h1').fudge('alarmClock');
